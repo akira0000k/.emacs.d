d437e51d8f973d10403dc11fd51e9074b4c78b06
akira0000k <95961660+akira0000k@users.noreply.github.com>
2023-09-05 17:44:10 +0900

    M-s-up/down for MacOSX
    M-s-up = M-home : show start of other window buffer
    M-s-down = M-end : show end of other window buffer

--- dot/02cursor.el
+++ dot/02cursor.el
@@ -48,6 +48,12 @@
 (global-set-key (kbd "ESC S-<down>") #'(lambda()(interactive)(scroll-other-window  4)))
 (global-set-key (kbd "M-S-<up>")     #'(lambda()(interactive)(scroll-other-window -4)))
 (global-set-key (kbd "ESC S-<up>")   #'(lambda()(interactive)(scroll-other-window -4)))
+(global-set-key (kbd "M-s-<up>")     'beginning-of-buffer-other-window)  ;;like M-home
+(global-set-key (kbd "ESC s-<up>")   'beginning-of-buffer-other-window)  ;;like M-home
+(global-set-key (kbd "M-s-<down>")   'end-of-buffer-other-window)        ;;like M-end
+(global-set-key (kbd "ESC s-<down>") 'end-of-buffer-other-window)        ;;like M-end
+;;   'scroll-other-window-down  M-<prior>
+;;   'scroll-other-window       M-<next>
 
 ;;====================================
 ;;;; go to top or end by page down/up
